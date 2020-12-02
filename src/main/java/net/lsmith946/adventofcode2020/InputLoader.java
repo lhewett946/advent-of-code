df48c424f08b1aa602d90d7bc90bfe3e142969fa
lsmith946 <lsmith946@gmail.com>
2020-12-02 08:32:22 +0000

    Add functionality to load strings to a list to the input loader

--- src/main/java/net/lsmith946/adventofcode2020/InputLoader.java
+++ src/main/java/net/lsmith946/adventofcode2020/InputLoader.java
@@ -2,19 +2,25 @@ package net.lsmith946.adventofcode2020;
 
 import java.io.*;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class InputLoader {
 
-    public Set<Integer> loadToIntSet (String resourcePath) throws IOException {
+    private BufferedReader openFileForReading(String resourcePath) throws FileNotFoundException {
         URL u = getClass().getResource(resourcePath);
         if (u == null) {
             throw new FileNotFoundException();
         }
         String filePath = getClass().getResource(resourcePath).getFile();
         File file = new File(filePath);
-        BufferedReader br = new BufferedReader(new FileReader(file));
+        return new BufferedReader(new FileReader(file));
+    }
+
+    public Set<Integer> loadToIntSet (String resourcePath) throws IOException {
+        BufferedReader br = openFileForReading(resourcePath);
         Set<Integer> ints = new HashSet<>();
 
         String str;
@@ -23,5 +29,17 @@ public class InputLoader {
         }
 
         return ints;
+    }
+
+    public List<String> loadToStringList (String resourcePath) throws IOException {
+        BufferedReader br = openFileForReading(resourcePath);
+        List<String> strings = new ArrayList<>();
+
+        String str;
+        while((str = br.readLine()) != null) {
+            strings.add(str);
+        }
+
+        return strings;
     }
 }
--- src/test/java/net/lsmith946/adventofcode2020/InputLoaderTest.java
+++ src/test/java/net/lsmith946/adventofcode2020/InputLoaderTest.java
@@ -4,7 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class InputLoaderTest {
@@ -23,5 +25,18 @@ public class InputLoaderTest {
         expectedInts.add(1456);
 
         Assert.assertEquals(expectedInts, loadedInts);
+    }
+
+    @Test
+    public void testLoadingStringsToList() throws IOException {
+        InputLoader il = new InputLoader();
+        List<String> loadedStrings = il.loadToStringList("/string_loader_test_data.txt");
+        List<String> expectedStrings = new ArrayList<>();
+
+        expectedStrings.add("1-3 a: abcde");
+        expectedStrings.add("1-3 b: cdefg");
+        expectedStrings.add("2-9 c: ccccccccc");
+
+        Assert.assertEquals(expectedStrings, loadedStrings);
     }
 }
