ab513069d78d96d1e03f5b3a0bb815b65db31884
lsmith946 <lsmith946@gmail.com>
2020-12-08 12:11:41 +0000

    Add JavaDocs to InputLoader class. Minor tweak to avoid code duplication.

--- src/main/java/net/lsmith946/adventofcode/utils/InputLoader.java
+++ src/main/java/net/lsmith946/adventofcode/utils/InputLoader.java
@@ -9,17 +9,31 @@ import java.util.Set;
 
 public class InputLoader {
 
+    /**
+     * The method opens the file specified by resourcePath for reading into a program
+     *
+     * @param resourcePath The path to the file to be read in, under resources
+     * @return
+     * @throws FileNotFoundException
+     */
     private BufferedReader openFileForReading(String resourcePath) throws FileNotFoundException {
         URL u = getClass().getResource(resourcePath);
         if (u == null) {
             throw new FileNotFoundException();
         }
-        String filePath = getClass().getResource(resourcePath).getFile();
+        String filePath = u.getFile();
         File file = new File(filePath);
         return new BufferedReader(new FileReader(file));
     }
 
-    public Set<Integer> loadToIntSet (String resourcePath) throws IOException {
+    /**
+     * This method loads the contents of the file specified by resourcePath to an integer set
+     *
+     * @param resourcePath The path to the file to be read in, under resources
+     * @return
+     * @throws IOException
+     */
+    public Set<Integer> loadToIntSet(String resourcePath) throws IOException {
         BufferedReader br = openFileForReading(resourcePath);
         Set<Integer> ints = new HashSet<>();
 
@@ -31,23 +45,37 @@ public class InputLoader {
         return ints;
     }
 
-    public List<String> loadToStringList (String resourcePath) throws IOException {
+    /**
+     * This method loads the contents of the file specified by resourcePath to a list of strings
+     *
+     * @param resourcePath The path to the file to be read in, under resources
+     * @return
+     * @throws IOException
+     */
+    public List<String> loadToStringList(String resourcePath) throws IOException {
         BufferedReader br = openFileForReading(resourcePath);
         List<String> strings = new ArrayList<>();
 
         String str;
-        while((str = br.readLine()) != null) {
+        while ((str = br.readLine()) != null) {
             strings.add(str);
         }
 
         return strings;
     }
 
+    /**
+     * This method loads the contents of the file specified by resourcePath to a single string
+     *
+     * @param resourcePath The path to the file to be read in, under resources
+     * @return
+     * @throws IOException
+     */
     public String loadToString(String resourcePath) throws IOException {
         BufferedReader br = openFileForReading(resourcePath);
         String str;
         String input = "";
-        while((str = br.readLine()) != null) {
+        while ((str = br.readLine()) != null) {
             input = input.concat(str);
         }
         return input;
