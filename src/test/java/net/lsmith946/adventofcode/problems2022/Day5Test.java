b6796bb902df45e24cb5a093368c23849a884bf0
Louisa Smith <lsmith946@gmail.com>
2023-03-30 21:19:50 +0100

    Don't do spellchecking on things that aren't words to clean up code inspection results

--- src/test/java/net/lsmith946/adventofcode/problems2022/Day5Test.java
+++ src/test/java/net/lsmith946/adventofcode/problems2022/Day5Test.java
@@ -11,12 +11,14 @@ public class Day5Test {
     @Test
     public void testDay5Part1() throws IOException {
         Day5 d5 = new Day5();
+        //noinspection SpellCheckingInspection
         Assertions.assertEquals("SVFDLGLWV", d5.solvePartOne());
     }
 
     @Test
     public void testDay5Part2() throws IOException {
         Day5 d5 = new Day5();
+        //noinspection SpellCheckingInspection
         Assertions.assertEquals("DCVTCVPCL", d5.solvePartTwo());
     }
 }
