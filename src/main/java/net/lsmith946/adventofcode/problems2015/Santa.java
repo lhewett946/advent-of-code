cd5056ccd8ca673d47905c8b775d91d8aa5d9107
Louisa Smith <lsmith946@gmail.com>
2023-04-05 20:45:02 +0100

    Migrate Day 3 solution to make use of the new Santa class

--- src/main/java/net/lsmith946/adventofcode/problems2015/Santa.java
+++ src/main/java/net/lsmith946/adventofcode/problems2015/Santa.java
@@ -1,11 +1,23 @@
 package net.lsmith946.adventofcode.problems2015;
 
+import net.lsmith946.adventofcode.utils.CompassDirections;
 import net.lsmith946.adventofcode.utils.Counter;
 import net.lsmith946.adventofcode.utils.CounterOperations;
+import net.lsmith946.adventofcode.utils.Grid2DCoordinates;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Santa {
 
     private final Counter currentFloor;
+
+    public Set<Grid2DCoordinates> getHousesVisited() {
+        return housesVisited;
+    }
+
+    private final Set<Grid2DCoordinates> housesVisited;
+    private Grid2DCoordinates currentPosition;
 
     public int getCurrentFloor() {
         return currentFloor.getCurrentValue();
@@ -17,6 +29,9 @@ public class Santa {
 
     public Santa() {
         this.currentFloor = new Counter();
+        this.housesVisited = new HashSet<>();
+        this.currentPosition = new Grid2DCoordinates(0, 0);
+        this.housesVisited.add(new Grid2DCoordinates(0, 0));
     }
 
     public void changeFloors(char direction) {
@@ -26,4 +41,21 @@ public class Santa {
             currentFloor.modifyCounter(CounterOperations.DECREMENT, 1);
         }
     }
+
+    private CompassDirections decodeDirections(char c) {
+        return switch (c) {
+            case '^' -> CompassDirections.NORTH;
+            case 'v' -> CompassDirections.SOUTH;
+            case '>' -> CompassDirections.EAST;
+            default -> CompassDirections.WEST;
+        };
+    }
+
+    public void addHouseLocation(char dir) {
+        CompassDirections direction = decodeDirections(dir);
+        Grid2DCoordinates newPosition = currentPosition.move(direction, 1);
+        housesVisited.add(newPosition);
+        currentPosition = newPosition;
+    }
+
 }
--- src/main/java/net/lsmith946/adventofcode/problems2015/days/Day3.java
+++ src/main/java/net/lsmith946/adventofcode/problems2015/days/Day3.java
@@ -1,12 +1,11 @@
 package net.lsmith946.adventofcode.problems2015.days;
 
-import net.lsmith946.adventofcode.utils.CompassDirections;
+import net.lsmith946.adventofcode.problems2015.Santa;
 import net.lsmith946.adventofcode.utils.Grid2DCoordinates;
 import net.lsmith946.adventofcode.utils.InputLoader;
 import net.lsmith946.adventofcode.utils.Puzzle;
 
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.Set;
 
 public class Day3 implements Puzzle<Integer> {
@@ -17,45 +16,26 @@ public class Day3 implements Puzzle<Integer> {
         this.input = InputLoader.loadToCharArray("/2015/day3_input.txt");
     }
 
-    private CompassDirections decodeDirections(char c) {
-        return switch (c) {
-            case '^' -> CompassDirections.NORTH;
-            case 'v' -> CompassDirections.SOUTH;
-            case '>' -> CompassDirections.EAST;
-            default -> CompassDirections.WEST;
-        };
-    }
-
-    private Set<Grid2DCoordinates> getVisitedLocations(char[] instructions) {
-        Grid2DCoordinates currentPosition = new Grid2DCoordinates(0, 0);
-        Set<Grid2DCoordinates> visitedHouses = new HashSet<>();
-        visitedHouses.add(new Grid2DCoordinates(0, 0));
-        for(char c : instructions) {
-            CompassDirections direction = decodeDirections(c);
-            Grid2DCoordinates newPosition = currentPosition.move(direction, 1);
-            visitedHouses.add(newPosition);
-            currentPosition = newPosition;
-        }
-        return visitedHouses;
-    }
-
     @Override
     public Integer solvePartOne() {
-        Set<Grid2DCoordinates> visitedHouses = getVisitedLocations(input);
-        System.out.println("Santa has visited " + visitedHouses.size() + " houses.");
-        return visitedHouses.size();
+        Santa santa = new Santa();
+        for(char c : input) {
+            santa.addHouseLocation(c);
+        }
+        System.out.println("Santa has visited " + santa.getHousesVisited().size() + " houses.");
+        return santa.getHousesVisited().size();
     }
 
     @Override
     public Integer solvePartTwo() {
-        char[] directionsSanta = new char[input.length/2];
-        char[] directionsRoboSanta = new char[input.length/2];
+        Santa santa = new Santa();
+        Santa roboSanta = new Santa();
         for(int i = 0; i < input.length/2; i++) {
-            directionsSanta[i] = input[i*2];
-            directionsRoboSanta[i] = input[(i*2)+1];
+            santa.addHouseLocation(input[i*2]);
+            roboSanta.addHouseLocation(input[(i*2)+1]);
         }
-        Set<Grid2DCoordinates> visitedHouses = getVisitedLocations(directionsSanta);
-        visitedHouses.addAll(getVisitedLocations(directionsRoboSanta));
+        Set<Grid2DCoordinates> visitedHouses = santa.getHousesVisited();
+        visitedHouses.addAll(roboSanta.getHousesVisited());
         System.out.println("Between them, Santa and Robo-Santa have visited " + visitedHouses.size() + " houses.");
         return visitedHouses.size();
     }
