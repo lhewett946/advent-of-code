1ae651c736c8e9af7313a924ba552596117dfd07
Louisa Smith <lsmith946@gmail.com>
2023-12-01 22:53:43 +0000

    Solution to 2023 Day 1 Part 2 with unit test

--- src/main/java/net/lsmith946/adventofcode/problems2023/days/Day1.java
+++ src/main/java/net/lsmith946/adventofcode/problems2023/days/Day1.java
@@ -6,6 +6,8 @@ import net.lsmith946.adventofcode.utils.Puzzle;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class Day1 implements Puzzle<Long> {
 
@@ -14,6 +16,21 @@ public final class Day1 implements Puzzle<Long> {
     public Day1() throws IOException {
         values = new ArrayList<>();
         this.values = InputLoader.loadToStringList("/2023/day1_input.txt");
+    }
+
+    private String toDigits (String str) {
+        return switch (str) {
+            case "one" -> "1";
+            case "two" -> "2";
+            case "three" -> "3";
+            case "four" -> "4";
+            case "five" -> "5";
+            case "six" -> "6";
+            case "seven" -> "7";
+            case "eight" -> "8";
+            case "nine" -> "9";
+            default -> str;
+        };
     }
 
     @Override
@@ -30,6 +47,29 @@ public final class Day1 implements Puzzle<Long> {
 
     @Override
     public Long solvePartTwo() {
-        return 0L;
+        Long sum = 0L;
+        Pattern findThese = Pattern.compile("[0-9]|one|two|three|four|five|six|seven|eight|nine");
+        for(String s : values) {
+            String processingString = s;
+            String selectedDigits = "";
+            String firstDigit = "", lastDigit = "";
+            while(!processingString.isEmpty()) {
+                Matcher matcher = findThese.matcher(processingString);
+                // get the first and last numbers
+                if(matcher.find()) {
+                    if (firstDigit.isEmpty()) {
+                        firstDigit = toDigits(matcher.group(0));
+                    }
+                    lastDigit = toDigits(matcher.group(0));
+                }
+                else break;
+                // strip the first character off the string
+                processingString = processingString.substring(1);
+            }
+            selectedDigits = firstDigit + lastDigit;
+            sum += Long.parseLong(selectedDigits);
+        }
+        System.out.println("The sum of all the calibration values is " + sum);
+        return sum;
     }
 }
--- src/test/java/net/lsmith946/adventofcode/problems2023/days/Day1Test.java
+++ src/test/java/net/lsmith946/adventofcode/problems2023/days/Day1Test.java
@@ -12,4 +12,10 @@ public class Day1Test {
         Day1 d1 = new Day1();
         Assertions.assertEquals(55712, d1.solvePartOne());
     }
+
+    @Test
+    public void testDay1Part2() throws IOException {
+        Day1 d1 = new Day1();
+        Assertions.assertEquals(55413, d1.solvePartTwo());
+    }
 }
