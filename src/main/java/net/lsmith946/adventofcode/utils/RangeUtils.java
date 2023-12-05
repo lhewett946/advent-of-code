d4e406e30c2e7978ed2923d94ed723d94b182413
Louisa Smith <lsmith946@gmail.com>
2023-12-05 23:36:39 +0000

    Create versions of RangeUtils functions that operate on long datatype instead of int datatype

--- src/main/java/net/lsmith946/adventofcode/utils/RangeUtils.java
+++ src/main/java/net/lsmith946/adventofcode/utils/RangeUtils.java
@@ -18,6 +18,21 @@ public class RangeUtils {
     }
 
     /**
+     * This function checks two ranges to see if they overlap with one another
+     * @param startRange1 The start index of the first range
+     * @param endRange1 The end index of the first range
+     * @param startRange2 The start index of the second range
+     * @param endRange2 The end index of the second range
+     * @return true if the ranges overlap, false if they do not
+     */
+    public static boolean rangesOverlap(long startRange1, long endRange1, long startRange2, long endRange2) {
+        return ((startRange1 >= startRange2) && (startRange1 <= endRange2)) ||
+                ((endRange1 >= startRange2) && (endRange1 <= endRange2)) ||
+                ((startRange2 >= startRange1) && (startRange2 <= endRange1)) ||
+                ((endRange2 >= startRange1) && (endRange2 <= endRange1));
+    }
+
+    /**
      * This functions checks two ranges to see if one of the ranges is fully contained within the other
      * @param startRange1 The start index of the first range
      * @param endRange1 The end index of the first range
@@ -26,6 +41,19 @@ public class RangeUtils {
      * @return true if one of the ranges is fully contained within the other, false otherwise
      */
     public static boolean rangesFullyContained(int startRange1, int endRange1, int startRange2, int endRange2) {
+        return (((startRange1 <= startRange2) && (endRange1 >= endRange2)) ||
+                ((startRange2 <= startRange1) && (endRange2 >= endRange1)));
+    }
+
+    /**
+     * This functions checks two ranges to see if one of the ranges is fully contained within the other
+     * @param startRange1 The start index of the first range
+     * @param endRange1 The end index of the first range
+     * @param startRange2 The start index of the second range
+     * @param endRange2 The end index of the second range
+     * @return true if one of the ranges is fully contained within the other, false otherwise
+     */
+    public static boolean rangesFullyContained(long startRange1, long endRange1, long startRange2, long endRange2) {
         return (((startRange1 <= startRange2) && (endRange1 >= endRange2)) ||
                 ((startRange2 <= startRange1) && (endRange2 >= endRange1)));
     }
